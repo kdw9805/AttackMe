bd8ecc977e1d7865553bd99e19d34c58115aa9fe
kdw9805 <35162059+kdw9805@users.noreply.github.com>
2022-05-17 05:13:14 +0900

    Update T1113-5.ps1

--- T1113-5.ps1
+++ T1113-5.ps1
@@ -3,4 +3,4 @@ $screen = [Windows.Forms.SystemInformation]::VirtualScreen
 $bitmap = New-Object Drawing.Bitmap $screen.Width, $screen.Height
 $graphic = [Drawing.Graphics]::FromImage($bitmap)
 $graphic.CopyFromScreen($screen.Left, $screen.Top, 0, 0, $bitmap.Size)
-$bitmap.Save("#{output_file}")
+$bitmap.Save("$env:TEMP\T1113.png")
