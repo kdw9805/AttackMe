9d40f22ef5c9fc5adba85ba6626e9102765ae93c
kdw9805 <35162059+kdw9805@users.noreply.github.com>
2022-10-23 16:35:42 +0900

    Update M2_v1.py

--- M2_v1.py
+++ M2_v1.py
@@ -11,7 +11,6 @@ info_List = []
 m_dict = {'NAME': '', 'IP': '', 'INFO': ''}
 hostname = subprocess.check_output(['hostname'])
 hostname = hostname.decode('utf-8')
-hostname = hostname.replace('\n', '')
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
